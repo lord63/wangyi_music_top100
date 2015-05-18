9800e90ae00807ca1a2c4b48b8bd213430e00b90
lord63 <lord63.j@gmail.com>
2015-05-18 10:07:27 +0800

    Add logging

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -6,12 +6,19 @@ from __future__ import absolute_import, unicode_literals
 import re
 import time
 from functools import reduce
+import logging
+import logging.config
 
 import requests
 from lxml import html
 import redis
+import yaml
 
 redis_server = redis.StrictRedis(host='localhost', port=6379)
+with open('logging.yaml') as f:
+    logging_config = yaml.load(f)
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger('crawler')
 
 
 def extract_num(raw):
@@ -57,17 +64,20 @@ def crawl_detailed_page(url):
 
 def crawl_the_page(url):
     """Crawl all the songlists in one page."""
+    logger.info('Start crawl the page: {0}'.format(url))
     base_url = 'http://music.163.com'
     tree = html.fromstring(requests.get(url).text)
 
     for songlist in tree.cssselect('.u-cover > a'):
         crawl_detailed_page(base_url + songlist.get('href'))
+        logger.info('Crawled one songlist: {0}'.format(songlist.get('href')))
 
     # Take a rest, to avoid being banned.
     time.sleep(5)
 
     next_page = tree.cssselect('.znxt')[0].get('href')
     if next_page != 'javascript:void(0)':
+        logger.info('Crawl next page: {0}'.format(next_page))
         crawl_the_page(base_url + next_page)
 
 
@@ -81,6 +91,7 @@ def generate_ranklists():
                       desc=True, store='favourites_rank')
     redis_server.sort('songlists', start=0, num=100, by='*->shares',
                       desc=True, store='shares_rank')
+    logger.info('Generate four ranklists.')
     # Union four top rank lists to get final toplist we will maintain
     # to update regularly.
     toplist = reduce(lambda a, b: set(a).union(b), [
@@ -90,11 +101,12 @@ def generate_ranklists():
         redis_server.lrange('shares_rank', 0, -1)])
     redis_server.delete('toplist')  # Clear the old toplist.
     redis_server.lpush('toplist', *toplist)
+    logger.info('Generate the toplist.')
 
     # Clean up, remove unused songlist.
     for songlist in set(redis_server.lrange('songlists', 0, -1)).difference(
             redis_server.lrange('toplist', 0, -1)):
         redis_server.delete(songlist)
-
     # Remove ununsed songlist in "songlists" list.
     redis_server.sort('toplist', alpha=True, store='songlists')
+    logger.info('Clean up, remove unused songlists.')
