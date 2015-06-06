e9ac971bbd361c0f68f133b706626154436ab49b
lord63 <lord63.j@gmail.com>
2015-06-06 10:42:58 +0800

    We can filter the filted songlists

--- static/wangyi.js
+++ static/wangyi.js
@@ -1,15 +1,8 @@
 $('.badge').on('click', function() {
-        var filted_list, trs;
-        filted_list = [];
-        trs = $('tr');
-        for (var i = 0; i < trs.length; i++) {
-            if ($('tr').eq(i).children().last().text().indexOf($(this).text()) !== -1) {
-                filted_list.push($('tr').eq(i).html());
-            }
-        }
-        $('tbody').empty();
-        for (var j = 0; j < filted_list.length; j++) {
-            $('tbody').append('<tr>' + filted_list[j] + '</tr>');
+    var trs = $('tr');
+    for (var i = 1; i < trs.length; i++) {
+        if (trs.eq(i).children().last().text().indexOf($(this).text()) === -1) {
+            trs.eq(i).empty();
         }
     }
-);
+});
