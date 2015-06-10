0501885d944f81f0aaad28a093443a5cd4fe932c
lord63 <lord63.j@gmail.com>
2015-06-10 16:38:48 +0800

    Improve tag filter, #1

--- static/wangyi.js
+++ static/wangyi.js
@@ -1,8 +1,32 @@
+filter = function (items, filter) {
+    for (var i = 0; i < items.length; i++) {
+        if (items.eq(i).children().last().text().indexOf(filter) === -1) {
+            items.eq(i).hide();
+        }
+    }
+};
+
+// Click and filter the songlists.
 $('.badge').on('click', function() {
-    var trs = $('tr');
-    for (var i = 1; i < trs.length; i++) {
-        if (trs.eq(i).children().last().text().indexOf($(this).text()) === -1) {
-            trs.eq(i).empty();
+    var trs;
+    trs = $('tbody tr:visible');
+    if ($('.undo').text().indexOf($(this).text()) === -1) {
+        $('thead tr td').last().append('<span class="badge undo"><span>' + $(this).text() + '</span><span class="glyphicon glyphicon-remove-sign"></span></span>');
+        filter(trs, $(this).text());
+    }
+});
+
+// Undo the filter.
+$('td').on('click', 'span.undo', function() {
+    var i;
+    $(this).remove();
+    if ($('.undo').length == 0) {
+        $('tbody tr').show();
+    }
+    else {
+        $('tbody tr').show();
+        for (i=0; i<$('.undo').length; i++) {
+            filter($('tbody tr:visible'), $('.undo span').eq(2*i).text());
         }
     }
 });
